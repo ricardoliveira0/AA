e9c4b02445aecdfc9ebb21acadbbc05683896804
Ricardo <l42647@alunos.uevora.pt>
2023-07-27 02:15:03 +0100

    add: Test w/o training set

--- main.py
+++ main.py
@@ -34,3 +34,16 @@ for q, r in zip(n.array(x1), nba.predict(x1)):
     print(f'{q} --> {r}')
 print(f'Precisão: {nba.precision_score(x1, y1)}')
 print(f'Exatidão: {nba.accuracy_score(x1, y1)}')
+
+print("\t Teste3")
+nba = NaiveBayesUevora.NaiveBayesUevora()
+file = p.read_csv("assets/weather-nominal.csv")
+x = file.drop([file.columns[-1]], axis=1) # drop last column | 1 for column
+y = file[file.columns[-1]]
+nba.__init__(1)
+nba.fit(x, y)
+
+x1 = [["overcast", "mild", "high", "TRUE"]]
+print("Previsão:")
+for q, r in zip(n.array(x1), nba.predict(x1)):
+    print(f'{q} --> {r}')
